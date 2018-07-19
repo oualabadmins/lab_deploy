cf7e7dea4d33790e8a7683f049039291168b53e1
kelleyvice-msft <kvice@microsoft.com>
2018-07-19 10:42:55 -0700

    Updated for the max-base-config_x-vm template

--- max-base-config_x-vm/scripts/Deploy-TLG-X.ps1
+++ max-base-config_x-vm/scripts/Deploy-TLG-X.ps1
@@ -1,11 +1,12 @@
-﻿<#  Deploy-TLG-X.ps1
+﻿<#  Deploy-MAX-X.ps1
     Kelley Vice 7/17/2018
 
-    This script deploys the TLG (Test Lab Guide) 3 VM Base Configuration to your Azure RM subscription.
+    This script deploys the X VM Base Configuration to your Azure RM subscription.
     You must have the AzureRM PowerShell module installed on your computer to run this script.
     To install the AzureRM module, execute the following command from an elevated PowerShell prompt:
+        Install-Module AzureRM -Force
 
-    Install-Module AzureRM
+    7/19/2018 - Revised for the max-base-config_x-vm template
 
 #>
 
@@ -22,10 +23,10 @@ $adminPassword = "" # The administrator account password.
 $deployClientVm = "Yes" # Yes or No
 $numberOfAppVms = "1" # Number of app server VMs to deploy
 $numberOfClientVms = "1" # Number of client VMs to deploy
-$clientVhdUri = "" # The URI of the storage account containing the client VHD. Leave blank if you are not deploying a client VM.
 $vmSize = "Standard_DS2_v2" # Select a VM size for all server VMs in your deployment.
 $dnsLabelPrefix = "" # DNS label prefix for public IPs. Must be lowercase and match the regular expression: ^[a-z][a-z0-9-]{1,61}[a-z0-9]$.
-$_artifactsLocation = "https://raw.githubusercontent.com/oualabadmins/lab_deploy/master/tlg-base-config_3-vm" # Location of template artifacts.
+$subnetCIDR = "" # The CIDR subnet for your virtual network, i.e. 10.0.0.0/24.
+$_artifactsLocation = "https://raw.githubusercontent.com/oualabadmins/lab_deploy/master/max-base-config_3-vm" # Location of template artifacts.
 $_artifactsLocationSasToken = "" # Enter SAS token here if needed.
 $templateUri = "$_artifactsLocation/azuredeploy.json"
 
@@ -39,9 +40,9 @@ $parameters.Add("adminPassword",$adminPassword)
 $parameters.Add("deployClientVm",$deployClientVm)
 $parameters.Add("numberOfAppVms",$numberOfAppVms)
 $parameters.Add("numberOfClientVms",$numberOfClientVms)
-$parameters.Add("clientVhdUri",$clientVhdUri)
 $parameters.Add("vmSize",$vmSize)
 $parameters.Add("dnsLabelPrefix",$dnsLabelPrefix)
+$parameters.Add("subnetCIDR",$subnetCIDR)
 $parameters.Add("_artifactsLocation",$_artifactsLocation)
 $parameters.Add("_artifactsLocationSasToken",$_artifactsLocationSasToken)
 
