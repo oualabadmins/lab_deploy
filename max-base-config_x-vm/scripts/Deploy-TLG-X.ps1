0d1207abc1c56c2b93f96e11437983d12eb9c990
Kelley Vice <kvice@microsoft.com>
2018-07-24 13:30:29 -0700

    Updated script

--- max-base-config_x-vm/scripts/Deploy-TLG-X.ps1
+++ max-base-config_x-vm/scripts/Deploy-TLG-X.ps1
@@ -7,28 +7,30 @@
         Install-Module AzureRM -Force
 
     7/19/2018 - Revised for the max-base-config_x-vm template
+    7/24/2018 - Updated _artifactsLocation, added guidance for params
 
 #>
 
 # Provide parameter values
-$subscription = "subscription name"
-$resourceGroup = "resource group name"
-$location = "location, i.e. West US"
+$subscription = "MAXLAB R&D Sandbox" # Use MAXLAB R&D Sandbox, EXT 1 or EXT 2.
+$resourceGroup = "resource group name" # Specify a unique resource group name.
+$location = "location, i.e. West US" # West US and West US 2 will provide the lowest latency.
 
 $configName = "" # The name of the deployment, i.e. BaseConfig01. Do not use spaces or special characters other than _ or -. Used to concatenate resource names for the deployment.
 $domainName = "" # The FQDN of the new AD domain.
 $serverOS = "2016-Datacenter" # The OS of application servers in your deployment, i.e. 2016-Datacenter or 2012-R2-Datacenter.
-$adminUserName = "" # The name of the domain administrator account to create, i.e. globaladmin.
+$adminUserName = "oualabadmin" # The name of the domain administrator account to create, i.e. oualabadmin.
 $adminPassword = "" # The administrator account password.
 $deployClientVm = "Yes" # Yes or No
 $numberOfAppVms = "1" # Number of app server VMs to deploy
 $numberOfClientVms = "1" # Number of client VMs to deploy
-$vmSize = "Standard_DS2_v2" # Select a VM size for all server VMs in your deployment.
+$vmSize = "Standard_DS4_v2" # Select a VM size for all server VMs in your deployment.
+$vmSizeClient = "Standard_DS4_v2" # Select a VM size for all client VMs in your deployment.
 $dnsLabelPrefix = "" # DNS label prefix for public IPs. Must be lowercase and match the regular expression: ^[a-z][a-z0-9-]{1,61}[a-z0-9]$.
-$subnetCIDR = "" # The CIDR subnet for your virtual network, i.e. 10.0.0.0/24.
-$_artifactsLocation = "https://raw.githubusercontent.com/oualabadmins/lab_deploy/master/max-base-config_3-vm" # Location of template artifacts.
+$subnetCIDR = "10.0.0.0/24" # The CIDR subnet for your virtual network, i.e. 10.0.0.0/24.
+$_artifactsLocation = "https://raw.githubusercontent.com/oualabadmins/lab_deploy/master/max-base-config_x-vm" # Location of template artifacts. DO NOT CHANGE
 $_artifactsLocationSasToken = "" # Enter SAS token here if needed.
-$templateUri = "$_artifactsLocation/azuredeploy.json"
+$templateUri = "$_artifactsLocation/azuredeploy.json" # DO NOT CHANGE
 
 # Add parameters to array
 $parameters = @{}
@@ -41,6 +43,7 @@ $parameters.Add("deployClientVm",$deployClientVm)
 $parameters.Add("numberOfAppVms",$numberOfAppVms)
 $parameters.Add("numberOfClientVms",$numberOfClientVms)
 $parameters.Add("vmSize",$vmSize)
+$parameters.Add("vmSizeClient",$vmSizeClient)
 $parameters.Add("dnsLabelPrefix",$dnsLabelPrefix)
 $parameters.Add("subnetCIDR",$subnetCIDR)
 $parameters.Add("_artifactsLocation",$_artifactsLocation)
