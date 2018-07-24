647bb22784b508e09d050cdb739ac62d83af09fa
Kelley Vice <kvice@microsoft.com>
2018-07-24 12:04:42 -0700

    Added params for vmName, UbuntuOsVersion; updated resource names

--- ubuntu-mate-desktop-vscode/README.md
+++ ubuntu-mate-desktop-vscode/README.md
@@ -1,18 +1,18 @@
-# Deploy an Ubuntu Mate Desktop VM with VSCode
+# Deploy an Ubuntu Mate Desktop VM with ROS Robotics
 
 Updated by kelleyvice-msft on 7/24/2018
 
-This template creates a Linux developer workstation as follows:
+This template creates a ROS developer workstation as follows:
 
-- Create a VM based on the Ubuntu 16.04 image with Mate Desktop installed
+- Create a VM based on the Ubuntu 16.04 image with Mate Desktop
 - Installs Azure CLI v2
 - Install Visual Studio Code editor
 - Install ROS Kinetic, Gazebo, RViz and MoveIt (robotics dev env)
 - Opens the RDP port for users to connect using remote desktop
 
-This template creates a new Ubuntu VM with Mate desktop enabled. Mate desktop is light weight and has a simple UI. In addition to a nice GUI, this template also installs developer tools like Azure CLI and Visual Studio Code for editing files. Users can connect to the Desktop UI using remote destop.
+This template creates a new Ubuntu VM with Mate desktop enabled. The template also installs developer tools for ROS robotics development. Users can connect to the Desktop UI using remote destop at port 3389.
 
-To connect, run "mstsc" from windows desktop and connect to the fqdn/public ip of the VM. 
+To connect, run "mstsc" from windows desktop and connect to the fqdn/public ip of the VM.
 
 <a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Foualabadmins%2Flab_deploy%2Fkvice-working%2Fubuntu-mate-desktop-vscode%2Fazuredeploy.json" target="_blank">
     <img src="http://azuredeploy.net/deploybutton.png"/>
--- ubuntu-mate-desktop-vscode/infra.sh
+++ ubuntu-mate-desktop-vscode/infra.sh
@@ -4,73 +4,50 @@ logger -t devvm "Install started: $?"
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
 
 apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
-
 apt-get install -y apt-transport-https
 
+# Install Azure CLI
 apt-get update && sudo apt-get install -y azure-cli
-
 logger -t devvm "Azure-cli installed: $?"
-
 sudo apt-get -y update
 
+# Install XRDP with Mate
 sudo apt-get -q=2 -y install xrdp
-
 logger -t devvm "XRDP installed: $?"
-
 logger -t devvm "Installing Mate Desktop ..."
-
 sudo dpkg --configure -a
-
 sudo apt-add-repository -y ppa:ubuntu-mate-dev/ppa
-
 sudo apt-add-repository -y ppa:ubuntu-mate-dev/trusty-mate
-
 sudo apt-get -y update
-
 sudo apt-get -y upgrade
-
 sudo apt-get install -q=2  --no-install-recommends -m ubuntu-mate-core
-
 sudo apt-get install -q=2  --no-install-recommends -m ubuntu-mate-desktop
-
 logger -t devvm "Mate Desktop installed. $?"
-
 echo mate-session >~/.xsession
-
 sudo service xrdp restart
 
-# FIxes the issue with Ubuntu desktop being blank.
-
+# Fixes issue with Ubuntu desktop being blank
 sudo sed -i -e 's/console/anybody/g' /etc/X11/Xwrapper.config
-
-
 logger -t devvm "Mate Desktop configured. $?"
-
 logger -t devvm "Installing VSCode: $?"
 
-
+# Install VSCode
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-
 sudo apt-get update
-
 sudo apt-get install -y code
-
 logger -t devvm "VSCode Installed: $?"
-
 logger -t devvm "Success"
 exit 0
 
-## kelley additions 7/24/2018
+## kvice additions 7/24/2018
 
-# Override artful update errors
+# Basic updates
+sed -i '.old' '/ppa.launchpad.net/{s/^/#/}' sources.list
 sudo apt upgrade --allow-unauthenticated
 sudo apt update --allow-unauthenticated
-
-# Install foundation applications
+sudo apt-get update && sudo apt-get install build-essential
 sudo apt-get install gedit
 
 # Install ROS Kinetic
@@ -92,7 +69,7 @@ catkin_make
 sudo apt-get update
 sudo apt-get install git-core python-argparse python-wstool python-vcstools python-rosdep ros-kinetic-control-msgs ros-kinetic-joystick-drivers ros-kinetic-xacro ros-kinetic-tf2-ros ros-kinetic-rviz ros-kinetic-cv-bridge ros-kinetic-actionlib ros-kinetic-actionlib-msgs ros-kinetic-dynamic-reconfigure ros-kinetic-trajectory-msgs ros-kinetic-rospy-message-converter
 
-# Install Intera Robto SDK
+# Install Intera Robotics SDK
 cd ~/ros_ws/src
 wstool init .
 git clone https://github.com/RethinkRobotics/sawyer_robot.git
@@ -104,8 +81,7 @@ catkin_make
 
 # Intera ROS environment setup
 cp ~/ros_ws/src/intera_sdk/intera.sh ~/ros_ws
-# Must now follow intera.sh ROS Environment Setup at http://sdk.rethinkrobotics.com/intera/Workstation_Setup
-
+# Now follow intera.sh ROS Environment Setup at http://sdk.rethinkrobotics.com/intera/Workstation_Setup
 
 # Install Gazebo
 sudo apt-get install gazebo7 ros-kinetic-qt-build ros-kinetic-gazebo-ros-control ros-kinetic-gazebo-ros-pkgs ros-kinetic-ros-control ros-kinetic-control-toolbox ros-kinetic-realtime-tools ros-kinetic-ros-controllers ros-kinetic-xacro python-wstool ros-kinetic-tf-conversions ros-kinetic-kdl-parser ros-kinetic-sns-ik-lib
@@ -123,7 +99,6 @@ source /opt/ros/kinetic/setup.bash
 cd ~/ros_ws
 catkin_make
 # Now follow Simulation at http://sdk.rethinkrobotics.com/intera/Gazebo_Tutorial
-
 
 # Install Rviz
 rosrun rviz rviz
