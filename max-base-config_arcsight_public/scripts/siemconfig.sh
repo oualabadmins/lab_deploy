a346ce748a8994bdb5aba68d1a57337ca1ebc88c
Kelley Vice <kvice@microsoft.com>
2018-09-12 13:36:47 -0700

    Remmed ESM-specific code until scripts are available

--- max-base-config_arcsight_public/scripts/siemconfig.sh
+++ max-base-config_arcsight_public/scripts/siemconfig.sh
@@ -37,26 +37,26 @@ systemctl enable xrdp.service
 yum -y update tzdata
 timedatectl set-timezone America/Los_Angeles
 
+##### FOLLOWING REMMED OUT UNTIL ARCSIGHT INSTALL SCRIPTS ARE AVAILABLE #####
+
 ## Install ArcSight EMS
 # From https://www.slideshare.net/Protect724/esm-install-guide60c
 
-# Disable IPv6 per https://community.softwaregrp.com/t5/ArcSight-User-Discussions/Fresh-ESM-Installation-stops-at-quot-Set-up-ArcSight-Storage/td-p/1519539
-sysctl -w net.ipv6.conf.default.disable_ipv6=1
-sysctl -w net.ipv6.conf.all.disable_ipv6=1
-
 # Install dependencies
-yum -y groupinstall "Web Server", "Compatibility Libraries", "Development Tools"
-yum -y install pam
+#yum -y groupinstall "Web Server", "Compatibility Libraries", "Development Tools"
+#yum -y install pam
 
 # Create arcsight user
-groupadd arcsight
-username="arcsight"
-password="$3rv1c3"
-pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-useradd -c “arcsight_software_owner” -g arcsight -d -p $pass $username
-/home/arcsight -m -s /bin/bash arcsight
+#groupadd arcsight
+#username="arcsight"
+#password="$3rv1c3"
+#pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+#useradd -c “arcsight_software_owner” -g arcsight -d -p $pass $username
+#/home/arcsight -m -s /bin/bash arcsight
 
-##### FOLLOWING REMMED OUT UNTIL ARCSIGHT INSTALL SCRIPTS ARE AVAILABLE #####
+# Disable IPv6 per https://community.softwaregrp.com/t5/ArcSight-User-Discussions/Fresh-ESM-Installation-stops-at-quot-Set-up-ArcSight-Storage/td-p/1519539
+#sysctl -w net.ipv6.conf.default.disable_ipv6=1
+#sysctl -w net.ipv6.conf.all.disable_ipv6=1
 
 # Create install folder, chown to arcsight
 #mkdir -m777 arcsight_install
@@ -71,3 +71,8 @@ useradd -c “arcsight_software_owner” -g arcsight -d -p $pass $username
 # Install ESM as arcsight user
 #su arcsight
 #./ArcSightESMSuite.bin -i console
+
+
+
+# Start services
+# /opt/arcsight/manager/bin/setup_services.sh
