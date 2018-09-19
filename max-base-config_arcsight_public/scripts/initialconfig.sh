13a090b376a1954298ae16ec7bf487c96c6da5d7
Kelley Vice <kvice@microsoft.com>
2018-09-19 12:01:14 -0700

    split script

---
+++ max-base-config_arcsight_public/scripts/initialconfig.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+## initialconfig.sh
+## kvice 9/12/2018
+## Configures a CentOS Linux host with xrdp, Mate, and other basic config tasks
+## Takes params: admin username $1, password $2
+
+# Set SELinux enforcement to permissive
+setenforce 0
+
+# Apply all available updates, omit Azure agent to prevent script failure
+yum update -y --exclude=WALinuxAgent
+
+# Install foundation packages
+yum install epel-release -y
+yum install byobu -y
+yum -y --enablerepo epel install xrdp tigervnc-server
+yum -y groupinstall "MATE Desktop"
+#yum groups install "Server with GUI" -y
+#yum groups install "MATE Desktop" -y
+#yum -y --enablerepo epel install mate-desktop
+#yum -y install mailx tcpdump
+
+# Set GUI autostart
+systemctl isolate graphical.target
+systemctl set-default graphical.target
+
+# Configure SELinux for xrdp
+chcon --type=bin_t /usr/sbin/xrdp
+chcon --type=bin_t /usr/sbin/xrdp-sesman
+
+# Start and enable xrdp
+service xrdp start
+systemctl enable xrdp.service
+
+# Set up Mate desktop for builtin admin user
+echo "exec mate-session" > /etc/$1/.Xclients
+chmod 700 /etc/$1/.Xclients
+
+# Set up Mate desktop for all users
+echo "exec mate-session" > /etc/skel/.Xclients
+chmod 700 /etc/skel/.Xclients
+
+# Restart xrdp
+service xrdp restart
+
+# Start GUI
+systemctl start graphical.target
+
+# Update time zone
+yum -y update tzdata
+timedatectl set-timezone America/Los_Angeles
+
+# Mount data disk /dev/sdc1 as /arcsight
+parted /dev/sdc mklabel msdos
+parted -s -a optimal /dev/sdc mkpart primary xfs 1MiB 1000MiB
+mkfs.xfs -L /arcsight -q /dev/sdc1
+mkdir /arcsight
+mount /dev/sdc1 /arcsight
+cp /etc/fstab /etc/fstab.old
+echo -e "/dev/sdc1 /arcsight xfs defaults 0 0" >> /etc/fstab
+
+# Restart
+reboot
--- max-base-config_arcsight_public/scripts/siemconfig.sh
+++ max-base-config_arcsight_public/scripts/siemconfig.sh
@@ -2,55 +2,11 @@
 
 ## siemconfig.sh
 ## kvice 9/12/2018
-## Configures a CentOS Linux host with xrdp, Mate, and ArcSight EMS
+## Configures a CentOS Linux host with ArcSight EMS
 ## Takes params: admin username $1, password $2
 
 # Set SELinux enforcement to permissive
 setenforce 0
-
-# Apply all available updates, omit Azure agent to prevent script failure
-yum update -y --exclude=WALinuxAgent
-
-# Install foundation packages
-yum install epel-release -y
-yum install byobu -y
-#yum groups install "Server with GUI" -y
-#yum groups install "MATE Desktop" -y
-yum -y --enablerepo epel install xrdp tigervnc-server
-yum -y --enablerepo epel install mate-desktop
-#yum -y install mailx tcpdump
-
-# Set GUI autostart
-systemctl isolate graphical.target
-systemctl set-default graphical.target
-
-# Configure SELinux for xrdp
-chcon --type=bin_t /usr/sbin/xrdp
-chcon --type=bin_t /usr/sbin/xrdp-sesman
-
-# Start and enable xrdp
-service xrdp start
-systemctl enable xrdp.service
-
-# Set up Mate desktop for builtin admin user
-#echo "exec mate-session" > ~/.Xclients
-#chmod 700 ~/.Xclients
-
-# Set up Mate desktop for all users
-#echo "exec mate-session" > /etc/skel/.Xclients
-#chmod 700 /etc/skel/.Xclients
-
-# Restart xrdp
-service xrdp restart
-
-# Start GUI
-systemctl start graphical.target
-
-# Update time zone
-yum -y update tzdata
-timedatectl set-timezone America/Los_Angeles
-
-##### FOLLOWING REMMED OUT UNTIL ARCSIGHT INSTALL SCRIPTS ARE AVAILABLE #####
 
 ## Install ArcSight EMS
 # From https://www.slideshare.net/Protect724/esm-install-guide60c
@@ -61,15 +17,6 @@ timedatectl set-timezone America/Los_Angeles
 #pass=$(perl -e 'print crypt($ARGV[0], "password")' $2)
 #useradd -c “arcsight_software_owner” -g arcsight -d -p $pass $username
 #/home/arcsight -m -s /bin/bash arcsight
-
-# Mount data disk /dev/sdc1 as /arcsight
-parted /dev/sdc mklabel msdos
-parted -s -a optimal /dev/sdc mkpart primary xfs 1MiB 1000MiB
-mkfs.xfs -L /arcsight -q /dev/sdc1
-mkdir /arcsight
-mount /dev/sdc1 /arcsight
-cp /etc/fstab /etc/fstab.old
-echo -e "/dev/sdc1 /arcsight xfs defaults 0 0" >> /etc/fstab
 
 # Install dependencies
 #yum -y groupinstall "Web Server", "Compatibility Libraries", "Development Tools"
